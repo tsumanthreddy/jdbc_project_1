d178c395c050927103bc4d1be8efac8fcebcfcb3
T SUMAN <156163809+tsumanthreddy@users.noreply.github.com>
2025-05-06 09:31:56 +0530

    Update Program1.java
    Updated

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -4,6 +4,10 @@ public class Program1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World Program");
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter the Square Number: ");
+		int a=sc.nextInt();
+		System.out.println("Square of "+ a + " is: "+(a*a));
 	}
 
 }
