f39fc47f6c73eb8837f7a081d895d84e98491a3b
SumanT <st6832617@gmail.com>
2025-05-07 07:08:39 +0530

    Again updation Successfully

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -6,10 +6,12 @@ public class Program1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World Program");
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the Square Number: ");
-		int a=sc.nextInt();
-		System.out.println("Square of "+ a + " is: "+(a*a));
+		int a = sc.nextInt();
+		System.out.println("Square of " + a + " is: " + (a * a));
+
+		System.out.println("Queb of " + a + " is: " + (a * a * a));
 	}
 
 }
